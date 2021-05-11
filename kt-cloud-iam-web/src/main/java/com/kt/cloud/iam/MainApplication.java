b7eb335049cb40d513b4a061299aa7a9eec6072f
Javis <javischen9548@foxmail.com>
2021-05-11 17:28:41 +0800

    u

--- kt-cloud-iam-web/src/main/java/com/kt/cloud/iam/MainApplication.java
+++ kt-cloud-iam-web/src/main/java/com/kt/cloud/iam/MainApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.kt.iam.module.*.persistence.dao")
+@MapperScan(basePackages = "com.kt.cloud.iam.module.*.persistence.dao")
 @SpringBootApplication
 public class MainApplication {
 
