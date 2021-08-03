58301c93ca7a96827cfe6c89ec0aaf896b0ec877
Javis <javischen9548@foxmail.com>
2021-08-03 22:27:31 +0800

    u

--- kt-cloud-iam-web/src/main/java/com/kt/cloud/iam/IamApplication.java
+++ kt-cloud-iam-web/src/main/java/com/kt/cloud/iam/IamApplication.java
@@ -3,15 +3,17 @@ package com.kt.cloud.iam;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan(basePackages = "com.kt.cloud.iam.module.*.persistence.dao")
 @SpringBootApplication
 @EnableFeignClients
-public class MainApplication {
+@EnableDiscoveryClient
+public class IamApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(IamApplication.class, args);
     }
 
 }
