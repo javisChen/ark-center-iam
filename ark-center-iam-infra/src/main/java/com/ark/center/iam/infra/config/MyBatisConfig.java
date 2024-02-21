866c612ca67e4607d8794359dce7b2a685e24d55
Javis <javischen9548@foxmail.com>
2024-02-22 00:19:03 +0800

    update

--- ark-center-iam-infra/src/main/java/com/ark/center/iam/infra/config/MyBatisConfig.java
+++ ark-center-iam-infra/src/main/java/com/ark/center/iam/infra/config/MyBatisConfig.java
@@ -3,8 +3,8 @@ package com.ark.center.iam.infra.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@MapperScan(basePackages = "com.ark.center.iam.infra.*.repository.db")
+@MapperScan(basePackages = {"com.ark.center.iam.infra.*.repository.db", "com.ark.center.iam.infra.relation.db"})
 @Configuration
 public class MyBatisConfig {
-    
+
 }
